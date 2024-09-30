a48c036556b18da435baacededb9230dda739036
JustLevel <86537104+JustLevel@users.noreply.github.com>
2024-09-30 11:42:07 +0200

    Update acb_codes.lua

--- acb_codes.lua
+++ acb_codes.lua
@@ -1,16 +1,19 @@
 local codes = {
-  '20KLIKES!',
-  '25KLIKES!',
-  '30KLIKES!',
-  '50KLIKES!',
+  'PEPPERSTHEBEST!',
+  'DAVIDSTEBEST!',
+  'YAMISTHEBEST!',
   '60KLIKES!',
-  '10MVISITS!',
-  '20MVISITS!',
-  '30MVISITS!',
   '40MVISITS!',
   'UPDATE5!',
-  'SORRYFORTHATBUGMYBAD!',
-  '2KTWITTERFOLLOWS!'
+  'SUB2Joltzy!',
+  'SUB2D1SGUISED!',
+  'SUB2CONSUME!',
+  'SUB2WIRY!',
+  'SUB2TOADBOI!',
+  'SUB2RIJORO!',
+  'SUB2VALK!',
+  'SUB2ItsHappyYT1!',
+  'FOLLOWEXVAR!'
 }
 
 return codes
