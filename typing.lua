25bc401a43332d63899158af5770f243c3cec728
JustLevel <86537104+JustLevel@users.noreply.github.com>
2024-11-07 10:45:42 -0800

    Update typing.lua

--- typing.lua
+++ typing.lua
@@ -1,29 +1,39 @@
-    repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game:IsLoaded()
+
 if game.PlaceId == 12886143095 or game.PlaceId == 18583778121 then
+    local args = {
+        [1] = "GetGlobalData"
+    }
+    game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
 
-local args = {
-    [1] = "GetGlobalData"
-}
+    task.wait(0.4)
 
-game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
+    local args = {
+        [1] = "GetData"
+    }
+    game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
 
-task.wait(0.4)
+    task.wait(1)
 
-local args = {
-    [1] = "GetData"
-}
+    local args = {
+        [1] = "Play",
+        [2] = 0,
+        [3] = "True"
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InfiniteCastleManager"):FireServer(unpack(args))
+else
+    task.wait(3)
+    local unit = game:GetService("Players")[game.Players.LocalPlayer.Name].Slots.Slot1.Value
+    local args = {
+        [1] = unit,
+        [2] = CFrame.new(-164.9412384033203, 197.93942260742188, 15.210136413574219) * CFrame.Angles(-0, 0, -0)
+    }
+    game:GetService("ReplicatedStorage").Remotes.PlaceTower:FireServer(unpack(args))
 
-game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
+    while not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("EndGameUI") do
+        wait()
+    end
 
-task.wait(1)
-
-local args = {
-    [1] = "Play",
-    [2] = 0,
-    [3] = "True"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InfiniteCastleManager"):FireServer(unpack(args))
+    wait(4)
+    game:GetService("ReplicatedStorage").Remotes.TeleportBack:FireServer()
 end
-
-while not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("EndGameUI") do wait() end wait(4) game:GetService("ReplicatedStorage").Remotes.TeleportBack:FireServer()
