1687678fca53c372644450a9e52edf56282dc7a3
JustLevel <86537104+JustLevel@users.noreply.github.com>
2024-11-09 03:19:04 +0100

    Update typing.lua

--- typing.lua
+++ typing.lua
@@ -1,26 +1,30 @@
+task.wait() 
 repeat task.wait() until game:IsLoaded()
 
 if game.PlaceId == 12886143095 or game.PlaceId == 18583778121 then
-    local args = {
-        [1] = "GetGlobalData"
-    }
-    game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
+    repeat
+        local args = {
+            [1] = "GetGlobalData"
+        }
+        game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
 
-    task.wait(0.4)
+        task.wait(0.4)
 
-    local args = {
-        [1] = "GetData"
-    }
-    game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
+        local args = {
+            [1] = "GetData"
+        }
+        game:GetService("ReplicatedStorage").Remotes.InfiniteCastleManager:FireServer(unpack(args))
 
-    task.wait(1)
+        task.wait(1)
 
-    local args = {
-        [1] = "Play",
-        [2] = 0,
-        [3] = "True"
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InfiniteCastleManager"):FireServer(unpack(args))
+        local args = {
+            [1] = "Play",
+            [2] = 0,
+            [3] = "True"
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InfiniteCastleManager"):FireServer(unpack(args))
+
+    until game.PlaceId ~= 12886143095 and game.PlaceId ~= 18583778121
 else
     task.wait(3)
     local unit = game:GetService("Players")[game.Players.LocalPlayer.Name].Slots.Slot1.Value
