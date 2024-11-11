a57aaa21faa5d401f6dc1bec5e7ab1b5a0071e3d
JustLevel <86537104+JustLevel@users.noreply.github.com>
2024-11-11 20:32:34 +0100

    Create goombahub.lua

---
+++ goombahub.lua
@@ -0,0 +1,22 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+wait(math.random())
+
+local games = {
+    [33820338] = 'https://api.luarmor.net/files/v3/loaders/79ccd93e0af9d6067ef3c778deffdc13.lua', -- AR FREE
+}
+
+if games[game.CreatorId] then
+	task.wait(math.random())
+	if game.CreatorId == 33820338 then
+		print('in')
+		repeat 
+			loadstring(game:HttpGet(games[game.CreatorId]))()
+			wait(10)
+		until getgenv().GoombaHub ~= nil;
+	else
+		loadstring(game:HttpGet(games[game.CreatorId]))()
+	end
+end
