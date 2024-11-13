55dc0a106ad5ede9d108837e32fe4cc99fd87a2b
JustLevel <86537104+JustLevel@users.noreply.github.com>
2024-11-13 22:05:02 +0100

    Create ar_codes.lua

---
+++ ar_codes.lua
@@ -0,0 +1,21 @@
+local codes = {
+  '20Mvisits',
+  '10MVisits',
+  'SorryForFoodBug!',
+  'XMEGACODE',
+  'MegaZillas',
+  'MegaMozKing',
+  'MegaRlxSage',
+  '5mVisits',
+  '100kLikes',
+  '2MVisits',
+  '50KLikes',
+  '200kMembers',
+  '1MVisits',
+  'Release',
+  'MozKing',
+  'SubtoRlxsage',
+  'SubtoZillas'
+}
+
+return codes
