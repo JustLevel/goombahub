5dcadea4c145de25312eb00adbe10f78a030279a
JustLevel <86537104+JustLevel@users.noreply.github.com>
2024-12-16 22:51:07 +0100

    Update awtd_codes.lua

--- awtd_codes.lua
+++ awtd_codes.lua
@@ -2,11 +2,7 @@ local codes = {
   'DAY13X2RR',
   'DragonBallZUpdate',
   'Sorry4DelayDBZUpdate',
-  'SorryForDelay',
-  '100KLikes',
-  'Sorry4Bugs',
   'HeroSlayer',
-  'DeadP',
   'DreamUpdatePart2',
   'EclipseUpdate'
 }
