bec35639fdbefb30d50a7ca0d33f2f204fe418ef
JustLevel <86537104+JustLevel@users.noreply.github.com>
2024-12-25 18:36:33 +0100

    Update premiumgoomba.lua

--- premiumgoomba.lua
+++ premiumgoomba.lua
@@ -7,7 +7,7 @@ wait(math.random())
 local games = {
     [33820338] = 'https://api.luarmor.net/files/v3/loaders/b16e78543f4efbe5f9eca25340d77fbe.lua', -- AR
     [7381705] = 'https://api.luarmor.net/files/v3/loaders/59349c34a86ce8d8b52691bc8df9a94c.lua', -- FISCH
-    [9122948] = 'https://api.luarmor.net/files/v3/loaders/0dfc6280be300724ee6253abc203ab74.lua' -- AWTD
+    [9122948] = 'https://api.luarmor.net/files/v3/loaders/0dfc6280be300724ee6253abc203ab74.lua', -- AWTD
     [10611639] = 'https://api.luarmor.net/files/v3/loaders/746d25796edc1fb2a94ff13422ab229a.lua' -- AA
 }
 
