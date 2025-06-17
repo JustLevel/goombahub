6b9c30c3e6b455c53449054026b90dd3040133ba
JustLevel <86537104+JustLevel@users.noreply.github.com>
2025-06-17 15:32:58 +0200

    Update goombahub.lua

--- goombahub.lua
+++ goombahub.lua
@@ -10,7 +10,8 @@ local games = {
     [10611639] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua', -- AA Free
     [15022320] = 'https://api.luarmor.net/files/v3/loaders/de14a2cfc434b795c0c5c904bfc8be43.lua', -- TTD FREE
     [17219742] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua', -- AV FREE
-    [15009415] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua' -- ARX FREE
+    [15009415] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua', -- ARX FREE
+    [34063840] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua' -- AnimeRising FREE
 }
 
 if games[game.CreatorId] then
