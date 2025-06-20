ad8d21db1ddd9d36b5428b65acb012b99902e535
JustLevel <86537104+JustLevel@users.noreply.github.com>
2025-06-20 17:10:27 +0200

    Update goombahub.lua

--- goombahub.lua
+++ goombahub.lua
@@ -11,7 +11,8 @@ local games = {
     [15022320] = 'https://api.luarmor.net/files/v3/loaders/de14a2cfc434b795c0c5c904bfc8be43.lua', -- TTD FREE
     [17219742] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua', -- AV FREE
     [15009415] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua', -- ARX FREE
-    [34063840] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua' -- AnimeRising FREE
+    [34063840] = 'https://api.luarmor.net/files/v3/loaders/884122ebe7f689f7a9677fcf49a273e0.lua', -- AnimeRising FREE
+    [35812225] = 'https://api.luarmor.net/files/v3/loaders/a31ba914b0a1382dc36f9dadaa0f14ee.lua' -- Anime Shadow 2
 }
 
 if games[game.CreatorId] then
