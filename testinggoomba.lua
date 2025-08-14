058511fc1a66b7a144e93ff400c1611d40c6c03d
JustLevel <86537104+JustLevel@users.noreply.github.com>
2025-08-14 02:17:50 +0200

    Update testinggoomba.lua

--- testinggoomba.lua
+++ testinggoomba.lua
@@ -5,7 +5,8 @@ end
 wait(math.random())
 
 local games = {
-    [5292947] = 'https://api.luarmor.net/files/v3/loaders/0806617fdbaf4671e167f19cb8820c77.lua' -- AWTDX
+    [5292947] = 'https://api.luarmor.net/files/v3/loaders/0806617fdbaf4671e167f19cb8820c77.lua', -- AWTDX
+    [17219742] = 'https://api.luarmor.net/files/v3/loaders/0806617fdbaf4671e167f19cb8820c77.lua'
 }
 
 if games[game.CreatorId] then
