134ade8f4135bd02655cc65fef376c24fe47f4dc
JustLevel <86537104+JustLevel@users.noreply.github.com>
2025-08-20 10:54:06 +0200

    Create goombaIds.lua

---
+++ goombaIds.lua
@@ -0,0 +1,13 @@
+local games = {
+    [33820338] = '884122ebe7f689f7a9677fcf49a273e0', -- AR FREE
+    [9122948] = '884122ebe7f689f7a9677fcf49a273e0', -- AWTD FREE
+    [10611639] = '884122ebe7f689f7a9677fcf49a273e0', -- AA Free
+    [15022320] = 'de14a2cfc434b795c0c5c904bfc8be43', -- TTD FREE
+    [17219742] = '884122ebe7f689f7a9677fcf49a273e0', -- AV FREE
+    [15009415] = '884122ebe7f689f7a9677fcf49a273e0', -- ARX FREE
+    [34063840] = '884122ebe7f689f7a9677fcf49a273e0', -- AnimeRising FREE
+    [35812225] = 'a31ba914b0a1382dc36f9dadaa0f14ee', -- Anime Shadow 2
+    [5292947] = 'a31ba914b0a1382dc36f9dadaa0f14ee' -- ASTDX
+}
+
+return games[game.CreatorId] or nil
