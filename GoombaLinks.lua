196c2247edff283af752d8c27f92130e7b8b841a
JustLevel <86537104+JustLevel@users.noreply.github.com>
2025-08-20 11:01:08 +0200

    Create GoombaLinks.lua

---
+++ GoombaLinks.lua
@@ -0,0 +1,13 @@
+local key_links = {
+    [33820338] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AR FREE
+    [9122948] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AWTD FREE
+    [10611639] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AA Free
+    [15022320] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- TTD FREE
+    [17219742] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AV FREE
+    [15009415] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- ARX FREE
+    [34063840] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AnimeRising FREE
+    [35812225] = 'https://ads.luarmor.net/get_key?for=Anime_Shadow_2-ScHvwCwJsuHR', -- Anime Shadow 2
+    [5292947] = 'https://ads.luarmor.net/get_key?for=Anime_Shadow_2-ScHvwCwJsuHR' -- ASTDX
+}
+
+return key_links;
