7035cfca474bbd1a556a82dfae6a54463f6e8ab5
JustLevel <86537104+JustLevel@users.noreply.github.com>
2025-08-21 02:39:04 +0200

    Update GoombaLinks.lua

--- GoombaLinks.lua
+++ GoombaLinks.lua
@@ -1,13 +1,40 @@
 local key_links = {
-    [33820338] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AR FREE
-    [9122948] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AWTD FREE
-    [10611639] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AA Free
-    [15022320] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- TTD FREE
-    [17219742] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AV FREE
-    [15009415] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- ARX FREE
-    [34063840] = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv', -- AnimeRising FREE
-    [35812225] = 'https://ads.luarmor.net/get_key?for=Anime_Shadow_2-ScHvwCwJsuHR', -- Anime Shadow 2
-    [5292947] = 'https://ads.luarmor.net/get_key?for=Anime_Shadow_2-ScHvwCwJsuHR' -- ASTDX
+    [33820338] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=Free_Scripts-QliJkLVEYVnH'
+    }, -- AR FREE
+    [9122948] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=Free_Scripts-QliJkLVEYVnH'
+    }, -- AWTD FREE
+    [10611639] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=Free_Scripts-QliJkLVEYVnH'
+    }, -- AA Free
+    [15022320] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=Free_Scripts-QliJkLVEYVnH'
+    }, -- TTD FREE
+    [17219742] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=Free_Scripts-QliJkLVEYVnH'
+    }, -- AV FREE
+    [15009415] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=Free_Scripts-QliJkLVEYVnH'
+    }, -- ARX FREE
+    [34063840] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=AR_Script-rdNWLsEJVrDv',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=Free_Scripts-QliJkLVEYVnH'
+    }, -- AnimeRising FREE
+    [35812225] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=Anime_Shadow_2-ScHvwCwJsuHR',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=AS2_Lootlabs-iFdSQmEKPWJH'
+    }, -- Anime Shadow 2
+    [5292947] = {
+        linkvertise = 'https://ads.luarmor.net/get_key?for=Anime_Shadow_2-ScHvwCwJsuHR',
+        lootlabs = 'https://ads.luarmor.net/get_key?for=AS2_Lootlabs-iFdSQmEKPWJH'
+    } -- ASTDX
 }
 
 return key_links;
