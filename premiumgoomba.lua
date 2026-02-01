6c3de981e70c7624ce11e850ac7a6b46d3974d47
JustLevel <86537104+JustLevel@users.noreply.github.com>
2026-02-01 18:07:57 +0100

    Update premiumgoomba.lua

--- premiumgoomba.lua
+++ premiumgoomba.lua
@@ -22,6 +22,7 @@ local games = {
 	[33861560] = 'https://api.luarmor.net/files/v3/loaders/b903396774d0a11fadefd37e41c98c44.lua', -- UTD
 	[8204913] = 'https://api.luarmor.net/files/v3/loaders/db9c70e6f6933a5c4757c80fffefd51b.lua', -- UTD (OG)
 	[35958846] = 'https://api.luarmor.net/files/v4/loaders/3589a9e33afc5013b0a37580bf98802b.lua', -- AP
+	[35757133] = 'https://api.luarmor.net/files/v4/loaders/8701b873d7fe9f2b58fa6433e589aa35.lua', -- SoloHunters
 }
 
 if games[game.CreatorId] then
